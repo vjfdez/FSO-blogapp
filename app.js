a4976b57677a8b1dc9214cdbe93368caadc83148
vjfdez <fernandez.valentinjoel@gmail.com>
2024-05-01 01:23:41 -0300

    Refactored app.

---
+++ app.js
@@ -0,0 +1,36 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+
+//------DB Connection.
+mongoose.set('strictQuery', false);
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  });
+
+//------Middlewares.
+app.use(cors());
+app.use(express.static('dist'));
+app.use(express.json());
+app.use(middleware.reqLogger);
+
+//------Routes.
+app.use('/api/blogs', blogsRouter);
+
+//------Error handler.
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
